5dfc9d1c4f06630cfa1c291cbbc86b701cd2a8fc
William-Moreno <intrcptr@comcast.net>
2020-12-01 17:10:39 -0800

    changed clone element from ul to li

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ function HornImage(jsonObject){
 // TODO: Define prototype render method
 
 HornImage.prototype.render = function(){
-    const $newImageDiv = $('#photo-template').find('ul').clone();
+    const $newImageDiv = $('#photo-template').find('li').clone();
     
     $newImageDiv.find('h2').text(this.title);
     $newImageDiv.find('p').text(this.description);
@@ -28,7 +28,7 @@ HornImage.prototype.render = function(){
 
 
 // TODO: Use AJAX to get JSON file
-$.get('../data/page-1.json', 'json').then(imageGallery => {
+$.ajax('../data/page-1.json', 'json').then(imageGallery => {
     imageGallery.forEach(imageJSONObject => hornImages.push(new HornImage(imageJSONObject)));
     hornImages.forEach(image => image.render());
 });
